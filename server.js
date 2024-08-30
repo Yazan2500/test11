d38202a59828b7a6dca175d8bc4a17854e9feac5
Yazan2500 <yazanof25@gmail.com>
2024-08-31 02:26:36 +0400

    Add website files

--- server.js
+++ server.js
@@ -4,33 +4,30 @@ const TelegramBot = require('node-telegram-bot-api');
 
 // هنا ضع الـ API Token الخاص بالبوت
 const token = '7443158315:AAFpzzGdez00G16YrRvRh4LmxqONAsGIw_0';
-
-// إنشاء كائن البوت
 const bot = new TelegramBot(token, { polling: true });
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-// استخدام body-parser للتعامل مع بيانات JSON
 app.use(bodyParser.json());
 
-// معرف القناة التي ترغب في سحب الرسائل منها
-const channelId = '@my2bot5files';
+bot.on('message', (msg) => {
+    const chatId = msg.chat.id;
+    const messageText = msg.text;
 
-// معالجة الطلبات المرسلة من الموقع
-app.post('/sendCommand', (req, res) => {
-    const messageId = req.body.messageId;
-
-    bot.forwardMessage(req.body.chatId, channelId, messageId)
-        .then(() => {
-            res.json({ status: 'success' });
-        })
-        .catch((error) => {
-            console.error(`Error forwarding message ${messageId}:`, error);
-            res.status(500).json({ status: 'error', message: error.message });
-        });
+    if (messageText) {
+        bot.forwardMessage(chatId, '@my2bot5files', messageText)
+            .then(() => {
+                bot.sendMessage(chatId, 'تم إرسال الرسالة بنجاح.');
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+                bot.sendMessage(chatId, 'حدث خطأ عند إرسال الرسالة.');
+            });
+    } else {
+        bot.sendMessage(chatId, 'يرجى إرسال معرف الرسالة.');
+    }
 });
 
-// بدء الخادم
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
